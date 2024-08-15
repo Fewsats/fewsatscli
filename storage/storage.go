c07edf6d6ac12a273fb2f64cc1503248798723b9
Pol Alvarez <pol.avms@gmail.com>
2024-08-15 11:25:44 +0200

    storage: moved file struct to storage.go

--- storage/storage.go
+++ storage/storage.go
@@ -1,8 +1,26 @@
 package storage
 
 import (
+	"time"
+
 	"github.com/urfave/cli/v2"
 )
+
+type File struct {
+	ExternalID      string    `json:"external_id"`
+	Name            string    `json:"name"`
+	Description     string    `json:"description"`
+	L402URL         string    `json:"l402_url"`
+	Size            uint64    `json:"size"`
+	Extension       string    `json:"extension"`
+	MimeType        string    `json:"mime_type"`
+	CoverURL        string    `json:"cover_url"`
+	PriceInUsdCents uint64    `json:"price_in_cents"`
+	CreatedAt       time.Time `json:"created_at"`
+	UpdatedAt       time.Time `json:"updated_at"`
+	Tags            []string  `json:"tags"`
+	Status          string    `json:"status"`
+}
 
 func Command() *cli.Command {
 	return &cli.Command{
